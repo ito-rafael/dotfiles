6b4fda4835e194507a99c9d0fc3165a662717849
Rafael Ito <ito.rafael@gmail.com>
2022-05-16 10:26:22 -0300

    [scripts] i3_save_layout.sh: fix sed commands

--- scripts/i3_save_layout.sh
+++ scripts/i3_save_layout.sh
@@ -11,7 +11,7 @@ where:
 #------------------------
 # print help menu
 if [[ $1 == '-h' || $1 == '--help' ]]; then
-	printf "script to navigate to i3 workspaces and open specifics windows if woskpace is empty\n\n"
+	printf "script to save i3 layout\n\n"
 	echo "$usage"
 	exit
 #------------------------
@@ -22,30 +22,33 @@ elif ! [[ $1 =~ ^[0-9]{,1}$ ]]; then
 	exit
 else
     WORKSPACE=$1
+    FILENAME=$HOME/.config/i3/ws$WORKSPACE.json
     #=================================================
     # save workspace layout to file
-    i3-save-tree --workspace $WORKSPACE > $HOME/.config/i3/ws$WORKSPACE.json
+    i3-save-tree --workspace $WORKSPACE > $FILENAME
     #=================================================
-    ## edit 
-    ## editing the json file
-    #    # vim ~/.config/i3/wsN.json
-    #    --------------------------
-    #    # remove the first line:
-    #        // vim:ts=4:sw=4:et
-    #    # remove the "//" under 'swallows' section:
-    #       // "class": "^Thunar$",
-    #       // "instance": "^thunar$",
-    #       // "title": "^rafael\\ \\-\\ File\\ Manager$",
-    #    # delete 'window_role' item:
-    #       // "window_role": "^Thunar\\-1587310553\\-3422989574$"
-    #    # delete last trailing comma from 'swallow' section:
-    #       // "title": "^rafael\\ \\-\\ File\\ Manager$",
-    #    --------------------------
-    #    # script not workig: 
-    #    tail -n +2 ~/.config/i3/wsN.json | fgrep -v '// split' | sed 's|//||g' > ~/.config/i3/wsN.json
-    #
-    ##=================================================
-    ## restoring the layout
-    #i3-msg "workspace WS_NAME; append_layout $HOME/.config/i3/wsN.json"
-    ##=================================================
+    # editing the json file: vim ~/.config/i3/wsN.json
+    #--------------------------
+    # remove the first line:
+    # // vim:ts=4:sw=4:et
+    sed -i '/^\/\/ vim:/d' $FILENAME
+    #--------------------------
+    # delete 'window_role' item:
+    # // "window_role": "^browser$"
+    sed -i '/"window_role": "/d' $FILENAME
+    #--------------------------
+    # remove the "//" under 'swallows' section:
+    # // "class": "^Brave\\-browser$",
+    # // "instance": "^brave\\-browser$",
+    # // "machine": "^Y2P\\-ArchLinux$",
+    # // "title": "^New\\ Tab\\ \\-\\ Brave$",
+    sed -i 's/\/\/ \(".*\)/\1/' $FILENAME
+    #--------------------------
+    # delete last trailing comma from 'swallow' section:
+    # "title": "^New\\ Tab\\ \\-\\ Brave$",
+    sed -i 's/\("title": .*\),$/\1/' $FILENAME
+    #=================================================
+    # restoring the layout
+    # i3-msg "workspace WS_NAME; append_layout $HOME/.config/i3/wsN.json"
+    #=================================================
 fi
