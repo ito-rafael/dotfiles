f7223f9c081fa449a396fd9668682784ac60cad6
Rafael Ito <ito.rafael@gmail.com>
2022-06-13 12:31:28 -0300

    [scripts] mx_script.sh: update host IDs

--- scripts/mx_script.sh
+++ scripts/mx_script.sh
@@ -3,10 +3,10 @@
 HOST=$(solaar config "MX Master 3" change-host | grep change-host | awk -F':' '{print $2}')
 if [ $HOST = 'catuaba' ]
 then
-    solaar config 'MX Master 3' change-host 2;
-    solaar config 'MX Keys' change-host 2;
-elif [ $HOST = 'Y2P-ArchLinux' ]
-then
     solaar config 'MX Master 3' change-host 1;
     solaar config 'MX Keys' change-host 1;
+elif [ $HOST = 'Y2P-ArchLinux' ]
+then
+    solaar config 'MX Master 3' change-host 2;
+    solaar config 'MX Keys' change-host 2;
 fi
