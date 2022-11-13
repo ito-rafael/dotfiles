e2b925b7f880fdd9e773c620edf79f143626977d
Rafael Ito <ito.rafael@gmail.com>
2022-11-13 13:25:04 -0300

    [nvim] lua/plugins.lua: use packer to manage itself

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,15 @@
 vim.cmd [[packadd packer.nvim]]
 
+-- automatically run ":PackerCompile" whenever plugins.lua is updated
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
 end)
