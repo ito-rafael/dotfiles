b4e8bb1416409555f6f1a4ca578087247d0eb53d
Rafael Ito <ito.rafael@gmail.com>
2024-04-14 15:20:44 -0300

    [scripts] xremap-keeb-config: update script

--- scripts/xremap-keeb-config.sh
+++ scripts/xremap-keeb-config.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-'''
-This script is meant to be run on Sway. It starts xremap configuring it to
-listen specific devices. After that, it configures the model, layout, and
-variant, of these keyboards. The keyboards supported so far are:
-
-keyboards:
-  - ZSA Moonlander
-  - ZSA Planck EZ
-  - laptop native keyboards
-'''
-
-# start xremap
+## start xremap
 xremap \
     --device "ZSA Technology Labs Moonlander Mark I" \
+    --device "ZSA Technology Labs Planck EZ Glow" \
     --device "KMonad INKL output" \
     --device "AT Translated Set 2 keyboard" \
     --watch $HOME/.config/xremap/config.yml &
@@ -23,10 +13,17 @@ sleep 2
 
 # get xremap device identifier
 XREMAP_DEVICE=$(swaymsg -t get_inputs | jq -re '.. | select(type == "object") | select(.type == "keyboard") | select(.name | startswith("xremap")) | .identifier')
-echo "xremap device identifier: $XREMAP_DEVICE"
 
-# configure keyboard within sway
-swaymsg input $XREMAP_DEVICE xkb_model "pc105"
-swaymsg input $XREMAP_DEVICE xkb_layout "us"
-swaymsg input $XREMAP_DEVICE xkb_variant "intl"
-echo "xremap input device configured!"
+# 
+if [[ $XREMAP_DEVICE == "" ]]; then
+    echo "Error: couldn't find xremap device!"
+    exit 1
+else
+    echo "xremap device identifier: $XREMAP_DEVICE"
+    # configure keyboard within sway
+    swaymsg input $XREMAP_DEVICE xkb_model "pc105"
+    swaymsg input $XREMAP_DEVICE xkb_layout "us"
+    swaymsg input $XREMAP_DEVICE xkb_variant "intl"
+    echo "xremap input device configured!"
+    exit 0
+fi
