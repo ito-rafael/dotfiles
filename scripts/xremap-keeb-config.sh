8c9bc31ea6ccac3fc1539c67a4417606ae4498e0
Rafael Ito <ito.rafael@gmail.com>
2024-05-07 09:34:20 -0300

    [scripts] xremap-keeb-config.sh: add support to i3wm

--- scripts/xremap-keeb-config.sh
+++ scripts/xremap-keeb-config.sh
@@ -14,19 +14,33 @@ xremap \
 # delay to wait device assignment
 sleep 2
 
-# get xremap device identifier
-XREMAP_DEVICE=$(swaymsg -t get_inputs | jq -re '.. | select(type == "object") | select(.type == "keyboard") | select(.name | startswith("xremap")) | .identifier')
+#=================================================
+# get session type (i3/sway/tty)
+#=================================================
 
-# 
-if [[ $XREMAP_DEVICE == "" ]]; then
-    echo "Error: couldn't find xremap device!"
-    exit 1
-else
-    echo "xremap device identifier: $XREMAP_DEVICE"
-    # configure keyboard within sway
-    swaymsg input $XREMAP_DEVICE xkb_model "pc105"
-    swaymsg input $XREMAP_DEVICE xkb_layout "us"
-    swaymsg input $XREMAP_DEVICE xkb_variant "intl"
-    echo "xremap input device configured!"
-    exit 0
-fi
+case "${XDG_SESSION_TYPE}" in
+    "x11") 
+        xmodmap $HOME/.Xmodmap
+        exit 0 ;;
+    "wayland")
+        # try to get xremap device identifier
+        XREMAP_DEVICE=$(swaymsg -t get_inputs | jq -re '.. | select(type == "object") | select(.type == "keyboard") | select(.name | startswith("xremap")) | .identifier')
+        
+        # check if xremap device exists
+        if [[ $XREMAP_DEVICE == "" ]]; then
+            echo "Error: couldn't find xremap device!"
+            exit 1
+        else
+            echo "xremap device identifier: $XREMAP_DEVICE"
+        
+            # configure keyboard within sway
+            swaymsg input $XREMAP_DEVICE xkb_model "pc105"
+            swaymsg input $XREMAP_DEVICE xkb_layout "us"
+            swaymsg input $XREMAP_DEVICE xkb_variant "intl"
+            echo "xremap input device configured!"
+            exit 0
+        fi
+        ;;
+    "tty") exit 0 ;;
+    *)     exit 0 ;;
+esac
