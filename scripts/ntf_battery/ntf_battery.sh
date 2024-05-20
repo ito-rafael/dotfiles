267fc9904523088632c9a51b1e122fc6ce0c5a9b
Rafael Ito <ito.rafael@gmail.com>
2024-05-20 06:58:02 -0300

    [scripts] ntf_battery: monitor battery of laptops

---
+++ scripts/ntf_battery/ntf_battery.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# send notifications when battery is low
+while true; do
+    battery_level=$(acpi -b | tail -n1 | grep -oP '[0-9]+(?=%)')
+    battery_state=$(acpi -b | tail -n1 | grep -oP '(?<=: ).*(?=, [0-9]{1,3}%)')
+    #echo $battery_level
+    #echo $battery_state
+    if [ $battery_state == "Discharging" ]; then
+        #---------------------------------------
+        # Battery Half Charged: 50%
+        #---------------------------------------
+        if [ $battery_level -eq 50 ]; then
+        #if [ $battery_level -le 50 ]; then
+            notify-send \
+                --expire-time=5000 \
+                --urgency=normal \
+                --icon='~/config/scripts/ntf_battery/half.png' \
+                "Battery Discharging" \
+                "Level: ${battery_level}%"
+            paplay /usr/share/sounds/freedesktop/stereo/dialog-information.oga
+        #---------------------------------------
+        # Battery Low: 15%
+        #---------------------------------------
+        elif [ $battery_level -eq 15 ]; then
+            notify-send \
+                --expire-time=0 \
+                --urgency=CRITICAL \
+                --icon='~/config/scripts/ntf_battery/low.png' \
+                "Battery Low" \
+                "Level: ${battery_level}%"
+            paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+        #---------------------------------------
+        # Battery Critical: < 5%
+        #---------------------------------------
+        #elif [ $battery_level -le 5 ]; then
+        elif [ $battery_level -ge 5 ]; then
+            # adjust volume if it is less than 40%
+            volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | grep -Eo '[0-9]{1,2}%' | grep -Eo '[0-9]{1,2}' | head -n1)
+            if [ $volume -le 40 ]; then
+                pactl list sinks | grep 'Sink #' | grep -o '[0-9]*' | xargs -i pactl set-sink-volume {} 50% && pkill -RTMIN+1 i3blocks
+            fi
+            # send notification
+            notify-send \
+                --expire-time=0 \
+                --urgency=CRITICAL \
+                --icon='~/config/scripts/ntf_battery/critical.png' \
+                "Battery Critical" \
+                "Level: ${battery_level}%"
+            # play sound until battery is charging
+            while [ $battery_state != "Charging" ]; do
+
+                battery_state=$(acpi -b | tail -n1 | grep -oP '(?<=: ).*(?=, [0-9]{1,3}%)')
+                paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+                sleep 10
+            done
+        fi
+        #---------------------------------------
+    fi
+    sleep 30
+done
