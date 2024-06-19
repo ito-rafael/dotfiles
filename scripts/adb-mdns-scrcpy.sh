2e89b7b5b2ecbd8c95c35a79e7ce315591fdaae7
Rafael Ito <ito.rafael@gmail.com>
2024-06-19 09:51:52 -0300

    [scripts] adb-mdns-scrcpy.sh: script to run scrcpy automatically as scratchpad

---
+++ scripts/adb-mdns-scrcpy.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#
+# This script is meant to be used with i3wm to mirror a device (eg: phone, smartwatch) screen on the desktop.
+#
+# Prerequisites:
+#   - device already paired with the desktop: "adb pair IP:PORT".
+#   - "debug wireless" activated on the device (Developer options).
+#   - scrcpy: tool to mirror device screen (https://github.com/Genymobile/scrcpy).
+#   - Avahi: tool to discover the IP & port of the device (wireless ADB) via mDNS.
+#
+# How it works:
+#   1. Check if =scrcpy= scratchpad exists. If it does, show it and exit.
+#   2. If not, try to find the ADB IP & port of the device. 
+#   3. If it finds, start =scrcpy= and display it. If not, exit.
+#
+
+# check if scratchpad is already active
+SCRATCHPAD=$(swaymsg -t get_tree | jq -re '.. | select(type == "object") | select(.app_id == "scrcpy" and .name == "dropdown_scrcpy")')
+
+# display scratchpad if it's active, or try to launch if it isn't
+if [[ $SCRATCHPAD ]]; then
+    echo "Scratchpad found! Displaying it..."
+    swaymsg '[app_id="scrcpy" title="dropdown_scrcpy"] scratchpad show'
+    exit 0
+else
+    # try to discover IP & port of device ADB via mDNS
+    echo "Scratchpad not found! Searching for ADB IP & port on device..."
+    MDNS_OUTPUT=$(avahi-browse --all --ignore-local --resolve --terminate --parsable | grep adb-RQ8N400FVCP-bNfaUl | grep v=ADB_SECURE_SERVICE_VERSION | tail -1)
+    IP=$(echo $MDNS_OUTPUT | cut -d ";" -f8)
+    PORT=$(echo $MDNS_OUTPUT | cut -d ";" -f9)
+
+    # check if ADB is running
+    if [ -z "${MDNS_OUTPUT}" ]; then
+        echo "Device not found! Exiting."
+        exit 1
+    else
+        echo "Device found: trying to connect to $IP:$PORT."
+        adb connect $IP:$PORT
+        scrcpy -s $IP:$PORT --window-title="dropdown_scrcpy" >& /dev/null &
+
+        # wait for scrcpy window to be launched
+        SCRATCHPAD=$(swaymsg -t get_tree | jq -re '.. | select(type == "object") | select(.app_id == "scrcpy" and .name == "dropdown_scrcpy")')
+        if [ -z $SCRATCHPAD ]; then
+            echo "Waiting scratchpad to be launched."
+            while [[ $SCRATCHPAD ]]; do
+                sleep 0.2
+                echo "..."
+                SCRATCHPAD=$(swaymsg -t get_tree | jq -re '.. | select(type == "object") | select(.app_id == "scrcpy" and .name == "dropdown_scrcpy")')
+            done
+        fi
+
+        # display scratchpad
+        sleep 2
+        swaymsg '[app_id="scrcpy" title="dropdown_scrcpy"] scratchpad show'
+        exit 0
+    fi
+fi
