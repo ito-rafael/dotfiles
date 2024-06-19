4b776c7095870f544ff253d53ffbc4e1f9cbf326
Rafael Ito <ito.rafael@gmail.com>
2024-06-19 11:12:17 -0300

    [scripts] adb-mdns-scrcpy: add snippet to get calc height/width of window

--- scripts/adb-mdns-scrcpy.sh
+++ scripts/adb-mdns-scrcpy.sh
@@ -14,14 +14,22 @@
 #   3. If it finds, start =scrcpy= and display it. If not, exit.
 #
 
-# calc size of window (90% of full height)
+# calc height (int) of the window (90% of full height)
 # examples:
-#   - for 1920x1080: height = 972
-#   - for 3840x2160: height = 1944
+#   - for Full HD (1920x1080): height = 972
+#   - for 4K (3840x2160): height = 1944
 RESOLUTION=$(swaymsg -t get_outputs | jq '.[] | select(.focused==true).current_mode')
-RES_WIDTH=$(echo $RESOLUTION | jq '.width')
+#RES_WIDTH=$(echo $RESOLUTION | jq '.width')
 RES_HEIGHT=$(echo $RESOLUTION | jq '.height')
-WIN_HEIGHT=$(echo "0.9 * $RES_HEIGHT" | bc)
+WIN_HEIGHT=$(echo "0.9 * $RES_HEIGHT / 1" | bc)
+
+# calc width (int) of the window based on the resolution of the device
+# examples:
+#   - for 1440x3200:
+#     - width = 1440/3200 * $WIN_HEIGHT
+#       - Full HD: width = 437
+#       - 4K: width = 875
+WIN_WIDTH=$(echo "0.455 * $WIN_HEIGHT / 1" | bc)
 
 # check if scratchpad is already active
 SCRATCHPAD=$(swaymsg -t get_tree | jq -re '.. | select(type == "object") | select(.app_id == "scrcpy" and .name == "dropdown_scrcpy")')
