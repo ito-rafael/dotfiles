16bcee9a18a4df95f7c99b5a09de892850e0426b
Rafael Ito <ito.rafael@gmail.com>
2024-06-24 09:29:31 -0300

    [surfingkeys] org: set cursor navigation in visual mode

--- surfingkeys/surfingkeys.js
+++ surfingkeys/surfingkeys.js
@@ -5,6 +5,11 @@ api.map("e", "j");
 api.map("i", "k");
 api.map("o", "l");
 
+api.vmap("n", "h");
+api.vmap("e", "j");
+api.vmap("i", "k");
+api.vmap("o", "l");
+
 api.map("N", "S");
 api.map("O", "D");
 
