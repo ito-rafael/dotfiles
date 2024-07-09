a8f2189ab99c21689ce74d36f0b703ccf1cda84d
Rafael Ito <ito.rafael@gmail.com>
2024-07-09 14:13:39 -0300

    [polybar] launch: add multi-monitor support

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,6 +9,10 @@ killall -q polybar
 sleep 1
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar i3 2>&1 | tee -a /tmp/polybar.log & disown
+#polybar i3 2>&1 | tee -a /tmp/polybar.log & disown
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    #MONITOR=$m polybar --reload i3 &
+    MONITOR=$m FC_DEBUG=1 polybar --reload i3 &
+done
 
 echo "Polybar launched..."
