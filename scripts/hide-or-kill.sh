204a9fea6ae4d9df04d6ba15ec7ff09aa71f9151
Rafael Ito <ito.rafael@gmail.com>
2024-08-05 10:22:06 -0300

    [scripts] hide-or-kill: add support to temporary scratchpad

--- scripts/hide-or-kill.sh
+++ scripts/hide-or-kill.sh
@@ -4,6 +4,14 @@
 This script is used as a replacement for the "swaymsg kill" command. Instead of instantly killing a window, it first checks if the app_id is a scratchpad. If it is, it simply hides the scratchpad instead of killing it. If it is a regular window, then the window is killed normally.
 '''
 
+SCRATCHPAD_TEMP='/tmp/scratchpad_pid.tmp'
+# check if file exists
+if [ -f $SCRATCHPAD_TEMP ]; then
+    TEMP_PID=$(cat $SCRATCHPAD_TEMP)
+else
+    TEMP_PID=0
+fi 
+
 # identify environment
 case "${XDG_SESSION_TYPE}" in
     "x11")
@@ -32,7 +40,8 @@ is_scratchpad=$($WM_CMD -t get_tree | jq -re '.. | select(type == "object") | se
     .'$PROP_PREFIX''$PROP' == "dropdown_python" or
     .'$PROP_PREFIX''$PROP' == "scrcpy" and .'$PROP_PREFIX''$CAPTION' == "dropdown_scrcpy" or
     .'$PROP_PREFIX''$PROP' == "brave-music.youtube.com__-Default" or
-    .'$PROP_PREFIX''$PROP' == "Brave-browser" and .'$PROP_PREFIX''$INSTANCE' == "music.youtube.com"
+    .'$PROP_PREFIX''$PROP' == "Brave-browser" and .'$PROP_PREFIX''$INSTANCE' == "music.youtube.com" or
+    .pid == '$TEMP_PID'
     ')
 
 # decide whether to hide (if scratchpad) of kill the window
