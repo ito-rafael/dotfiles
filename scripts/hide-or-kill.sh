569ffdbfcd99ad64eb31e42414d58a5687eb14b0
Rafael Ito <ito.rafael@gmail.com>
2024-08-21 15:26:57 -0300

    [scripts] hide-or-kill: add support to temporary scratchpads

--- scripts/hide-or-kill.sh
+++ scripts/hide-or-kill.sh
@@ -2,15 +2,36 @@
 
 #This script is used as a replacement for the "swaymsg kill" command. Instead of instantly killing a window, it first checks if the app_id is a scratchpad. If it is, it simply hides the scratchpad instead of killing it. If it is a regular window, then the window is killed normally.
 
-SCRATCHPAD_TEMP='/tmp/scratchpad_pid.tmp'
-# check if file exists
-if [ -f $SCRATCHPAD_TEMP ]; then
-    TEMP_PID=$(cat $SCRATCHPAD_TEMP)
-else
-    TEMP_PID=0
+#=======================================
+# temporary scratchpads
+#=======================================
+# scratchpad tmp files
+SCRATCHPAD_TEMP_1='/tmp/scratchpad_pid_1.tmp'
+SCRATCHPAD_TEMP_2='/tmp/scratchpad_pid_2.tmp'
+SCRATCHPAD_TEMP_3='/tmp/scratchpad_pid_3.tmp'
+# set default PID to 0
+TEMP_PID_1=0
+TEMP_PID_2=0
+TEMP_PID_3=0
+#----------------------------------
+# temporary scratchpad #1 (comma)
+if [ -f $SCRATCHPAD_TEMP_1 ]; then
+    TEMP_PID_1=$(cat $SCRATCHPAD_TEMP_1)
 fi 
+#----------------------------------
+# temporary scratchpad #2 (period)
+if [ -f $SCRATCHPAD_TEMP_2 ]; then
+    TEMP_PID_2=$(cat $SCRATCHPAD_TEMP_2)
+fi 
+#----------------------------------
+# temporary scratchpad #3 (slash)
+if [ -f $SCRATCHPAD_TEMP_3 ]; then
+    TEMP_PID_3=$(cat $SCRATCHPAD_TEMP_3)
+fi
 
+#=======================================
 # identify environment
+#=======================================
 case "${XDG_SESSION_TYPE}" in
     "x11")
         WM_CMD="i3-msg"
@@ -18,11 +39,13 @@ case "${XDG_SESSION_TYPE}" in
         PROP="class"
         CAPTION="title"
         INSTANCE="instance"
+        PID_PROP="window"
         ;;
     "wayland")
         WM_CMD="swaymsg"
         PROP="app_id"
         CAPTION="name"
+        PID_PROP="pid"
         ;;
     "tty")
         exit 0
@@ -32,7 +55,9 @@ case "${XDG_SESSION_TYPE}" in
         ;;
 esac
 
+#=======================================
 # check if the app_id is one of the listed bellow
+#=======================================
 is_scratchpad=$($WM_CMD -t get_tree | jq -re '.. | select(type == "object") | select(.focused) |
     .'$PROP_PREFIX''$PROP' == "dropdown_terminal" or
     .'$PROP_PREFIX''$PROP' == "dropdown_python" or
@@ -42,7 +67,9 @@ is_scratchpad=$($WM_CMD -t get_tree | jq -re '.. | select(type == "object") | se
     .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "music.youtube.com" or
     .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "web.whatsapp.com" or
     .'$PROP_PREFIX''$PROP' == "keymapp" or .'$PROP_PREFIX''$PROP' == "Keymapp" or
-    .pid == '$TEMP_PID'
+    .'$PID_PROP' == '$TEMP_PID_1' or 
+    .'$PID_PRID' == '$TEMP_PID_2' or 
+    .'$PID_PRID' == '$TEMP_PID_3'
     ')
 
 # decide whether to hide (if scratchpad) of kill the window
