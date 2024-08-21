19c54a299ac0a0c03a9e0f3b9cfa470991c5ba1f
Rafael Ito <ito.rafael@gmail.com>
2024-08-21 15:35:53 -0300

    [scripts] adb-mdns-scrcpy: add support to Keymapp

--- scripts/adb-mdns-scrcpy.sh
+++ scripts/adb-mdns-scrcpy.sh
@@ -120,6 +120,7 @@ if [ $FOCUSED != "scrcpy" ]; then
         .'$PROP_PREFIX''$PROP' == "brave-web.whatsapp.com__-Default" or
         .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "music.youtube.com" or
         .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "web.whatsapp.com" or
+        .'$PROP_PREFIX''$PROP' == "keymapp" or .'$PROP_PREFIX''$PROP' == "Keymapp" or
         .'$ID_PROP' == '$TEMP_PID_1' or 
         .'$ID_PROP' == '$TEMP_PID_2' or 
         .'$ID_PROP' == '$TEMP_PID_3'
