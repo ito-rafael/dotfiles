75295ab7ac9e7f6bd5e973d2932d3ac7497d52fd
Rafael Ito <ito.rafael@gmail.com>
2024-08-23 08:42:49 -0300

    [scripts] wlprop: add selection/focused options

--- scripts/wlprop.sh
+++ scripts/wlprop.sh
@@ -31,18 +31,36 @@
 # - slurp
 # - awk
 
+CMD=${1:-selection}
+
 # Get the sway tree and store the output
 SWAY_TREE=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?)')
 
-# Invoke slurp to let the user select a window
-SELECTION=$(echo $SWAY_TREE | jq -r '.rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)
-
-# Extract the X, Y, Width, and Height from the selection
-X=$(echo $SELECTION | awk -F'[, x]' '{print $1}')
-Y=$(echo $SELECTION | awk -F'[, x]' '{print $2}')
-W=$(echo $SELECTION | awk -F'[, x]' '{print $3}')
-H=$(echo $SELECTION | awk -F'[, x]' '{print $4}')
-
-# Find the window matching the selection
-echo $SWAY_TREE | jq -r --argjson x $X --argjson y $Y --argjson w $W --argjson h $H \
-  '. | select(.rect.x == $x and .rect.y == $y and .rect.width == $w and .rect.height == $h)'
+case "${CMD}" in
+    #=============================
+    "selection")
+        # Invoke slurp to let the user select a window
+        SELECTION=$(echo $SWAY_TREE | jq -r '.rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)
+        #-----------------------------
+        # Extract the X, Y, Width, and Height from the selection
+        X=$(echo $SELECTION | awk -F'[, x]' '{print $1}')
+        Y=$(echo $SELECTION | awk -F'[, x]' '{print $2}')
+        W=$(echo $SELECTION | awk -F'[, x]' '{print $3}')
+        H=$(echo $SELECTION | awk -F'[, x]' '{print $4}')
+        #-----------------------------
+        # Find the window matching the selection
+        echo $SWAY_TREE | jq -r --argjson x $X --argjson y $Y --argjson w $W --argjson h $H \
+          '. | select(.rect.x == $x and .rect.y == $y and .rect.width == $w and .rect.height == $h)' \
+          | tee /tmp/window_prop.tmp
+        ;;
+    #=============================
+    "focused")
+        # get current focused window
+        PROPERTIES=$(swaymsg -t get_tree | jq -re '.. | select(type == "object") | select(.focused)')
+        echo $PROPERTIES | tee /tmp/window_prop.tmp
+        ;;
+    #=============================
+    *)
+        exit 0
+        ;;
+esac
