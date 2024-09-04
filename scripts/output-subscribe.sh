96d8f01558d26459bb53dac8011db615e1f61c37
Rafael Ito <ito.rafael@gmail.com>
2024-09-04 11:32:13 -0300

    [scripts] output-subscribe: add support to Sway

--- scripts/output-subscribe.sh
+++ scripts/output-subscribe.sh
@@ -4,8 +4,30 @@
 #
 
 #=================================================
-	
-i3-msg -t subscribe -m '[ "window" ]' | while read line ; do 
-    FOCUSED_OUTPUT=$(echo $line | jq -re '.container.output')
+# identify session (i3wm/Sway) and set vars accordingly
+#=================================================
+
+case "${XDG_SESSION_TYPE}" in
+    "x11")
+        WM_CMD="i3-msg"
+        OUTPUT_CMD=$(i3-msg -t get_workspaces | jq -re '.[] | select(.focused).output')
+        ;;
+    "wayland")
+        WM_CMD="swaymsg" 
+        OUTPUT_CMD=$(swaymsg -t get_outputs | jq -re '.[] | select(.focused).name')
+        ;;
+    "tty")
+        exit 0
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
+#=================================================
+# get focused output
+#=================================================
+$WM_CMD -t subscribe -m '["window"]' | while read line; do 
+    FOCUSED_OUTPUT=$OUTPUT_CMD
     echo $FOCUSED_OUTPUT
 done
