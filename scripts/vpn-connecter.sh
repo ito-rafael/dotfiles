f7feb865caae727c10ccd1697389d3a7f4b7f254
Rafael Ito <ito.rafael@gmail.com>
2024-09-07 20:13:14 -0300

    [scripts] vpn-connecter: connect to VPNs with CLI single cmd

---
+++ scripts/vpn-connecter.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+#
+# This script is used to connect/disconnect VPNs.
+#
+# Installation:
+#   Arch:
+#     - sudo pacman -S yad
+#
+#=================================================
+# help menu and usage message
+#=================================================
+usage="$(basename "$0") destination action [-h]
+
+where:
+    -h, --help      show this help text
+    destination     supported options so far includes: \"lcn\", \"unicamp\", \"lbic\", \"samsung\"
+    action          action to be performed, can be one of the three options:
+      \"connect\"       connects to the VPN
+      \"disconnect\"    disconnects from the VPN
+      \"toggle\"        toggle the connection (connect/disconnect)
+"
+#=================================================
+# print help menu
+if [[ $1 == '-h' || $1 == '--help' || $1 == '' ]]; then
+    printf "Script to connect to or disconnect from VPNs.\n\n"
+    echo "$usage"
+    exit
+fi
+
+#=================================================
+# parse parameters
+VPN=$1
+ACTION=$2
+PID_FILE='/tmp/vpn_'${VPN}'_pid.tmp'
+LAUNCHER=${XDG_CONFIG_HOME}'/scripts/vpn/'${VPN}'/vpn-'${VPN}'.sh'
+
+# check if VPN is supported
+if ! [[ $VPN == 'lcn' || $VPN == 'unicamp' || $VPN == 'lbic' || $VPN == 'samsung' ]]; then
+    echo VPN option not supported. Exiting...
+    exit 1
+fi
+
+# check if action is supported
+if ! [[ $ACTION == 'connect' || $ACTION == 'disconnect' || $ACTION == 'toggle' ]]; then
+    echo Action option not supported. Exiting...
+    exit 1
+fi
+
+#=================================================
+# check option selected
+case "${ACTION}" in
+    "connect")
+        # check if it's already running
+        if [ -f $PID_FILE ]; then
+            # ignore
+            echo "PID file already exists. Exiting..."
+            exit 0
+        else
+           # get password with dialog
+           PASSWORD="$(yad --center --image=/usr/share/icons/Papirus-Dark/32x32/status/dialog-password.svg --entry --hide-text --title="Authentication required" --text="Enter password:")"
+            # start application and save its PID to file
+            echo "Starting application..."
+            echo $PASSWORD | sudo -S $LAUNCHER &
+            sleep 0.1
+            PID=$(ps --ppid $! -o pid=)
+            sudo echo $PID > $PID_FILE
+            exit 0
+        fi
+        ;;
+    "disconnect")
+        # check if it's already running
+        if [ -f $PID_FILE ]; then
+            # stop and remove PID file
+            echo "Stopping application..."
+            PID=$(cat "$PID_FILE")
+            rm $PID_FILE
+            kill $PID
+            exit 0
+        else
+            # ignore
+            echo "PID file does not exist. Exiting..."
+            exit 0
+        fi
+        ;;
+    "toggle")
+        # check if it's already running
+        if [ -f $PID_FILE ]; then
+            # stop and remove PID file
+            echo "Stopping application..."
+            PID=$(cat "$PID_FILE")
+            rm $PID_FILE
+            kill $PID
+            exit 0
+        else
+           # get password with dialog
+           PASSWORD="$(yad --center --image=/usr/share/icons/Papirus-Dark/32x32/status/dialog-password.svg --entry --hide-text --title="Authentication required" --text="Enter password:")"
+            # start application and save its PID to file
+            echo "Starting application..."
+            echo $PASSWORD | sudo -S $LAUNCHER &
+            sleep 0.1
+            PID=$(ps --ppid $! -o pid=)
+            sudo echo $PID > $PID_FILE
+            exit 0
+        fi
+        ;;
+    *)
+        echo "Option not supported. Exiting..."
+        exit 1
+        ;;
+esac
