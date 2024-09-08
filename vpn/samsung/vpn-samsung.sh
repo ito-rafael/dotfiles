fbd7e1ea70f275e2faf7b47d8a748034899bc89d
Rafael Ito <ito.rafael@gmail.com>
2024-09-08 19:24:33 -0300

    [vpn] samsung: new dir created (add Samsung connection script)

---
+++ vpn/samsung/vpn-samsung.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# input info
+USERNAME=<USER>
+HOST=<VPN-DOMAIN>
+PASSWD=$(gpg --decrypt ~/.config/scripts/openconnect/<ENCRYPTED-FILE>.gpg 2>/dev/null)
+TOKEN=$(stoken tokencode)
+
+# create tun device
+sudo ip tuntap add vpn-samsung mode tun user rafael
+
+# connect to VPN
+sudo openconnect $HOST \
+    --interface=vpn-samsung \
+    --script='sudo -E /etc/vpnc/vpnc-script' \
+    --form-entry=main:username=$USERNAME \
+    --form-entry=main:password=$PASSWD \
+    --form-entry=main:secondary_password=$TOKEN
+
+# delete device on exit
+del_device(){
+    sudo ip link del vpn-samsung
+}
+trap 'del_device' EXIT
