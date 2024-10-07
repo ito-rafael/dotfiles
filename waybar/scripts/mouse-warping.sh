cc5bb8ac79614b9330f5a5a6d5aa0b4002524ca8
Rafael Ito <ito.rafael@gmail.com>
2024-10-07 09:31:07 -0300

    [waybar] scripts: add mouse-warping ctl

---
+++ waybar/scripts/mouse-warping.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# read mouse warping status
+FILE="/tmp/mouse_warping.tmp"
+MOUSE_WARPING=$(cat $FILE)
+
+# parse parameter
+CMD=$1
+
+case "${CMD}" in
+    "monitor")
+        # set initial state
+        if [[ "$MOUSE_WARPING" == "1" ]]; then
+            echo '{"text": "", "class": "enabled"}'
+        else
+            echo '{"text": ""}'
+        fi
+
+        # infinite loop that changes text everytime the mouse warping file changes
+        inotifywait --quiet --monitor --event close_write $FILE | while read; do
+            MOUSE_WARPING=$(cat $FILE)
+            if [[ "$MOUSE_WARPING" == "1" ]]; then
+                echo '{"text": "", "class": "enabled"}'
+            else
+                echo '{"text": ""}'
+            fi
+            sleep 0.1
+        done
+        ;;
+    "toggle")
+        if [[ "$MOUSE_WARPING" == "1" ]]; then
+            # disable mouse warping
+            swaymsg mouse_warping none
+            echo 0 > $FILE
+        else
+            # enable mouse warping
+            swaymsg mouse_warping container
+            echo 1 > $FILE
+        fi
+        ;;
+    *)
+        ;;
+esac
