40b22fa06af31cd6a3e1fdc20be2605140b13eeb
Rafael Ito <ito.rafael@gmail.com>
2024-10-07 09:37:03 -0300

    [nvim] init.lua: add OSC 52 support (yank to clipboard over SSH)

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,15 @@ vim.cmd.source(vimrc)
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 end)
+-- OSC 52: yank to clipboard over SSH
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
