ffb9718a308d6483c20daaf25d3aed2ea98a0143
Rafael Ito <ito.rafael@gmail.com>
2024-10-11 11:17:35 -0300

    [waybar] script: add python scratchpad

---
+++ waybar/scripts/scratchpad-python.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# identify session (i3wm/Sway) and set vars accordingly
+case "${XDG_SESSION_TYPE}" in
+    "x11")
+        WM_CMD="i3-msg"
+        PROP_PREFIX="window_properties."
+        PROP="class"
+        CAPTION="title"
+        INSTANCE="instance"
+        ;;
+    "wayland")
+        WM_CMD="swaymsg"
+        PROP="app_id"
+        CAPTION="name"
+        ;;
+    "tty")
+        exit 0
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
+# check if scratchpad exists
+SCRATCHPAD=$($WM_CMD -t get_tree | jq -re '.. | select(type == "object") | select(
+    .'$PROP_PREFIX''$PROP' == "dropdown_python"
+    )')
+
+# decide if icon will be displayed in color or black/white
+if [[ $SCRATCHPAD ]]; then
+    printf '{"text": "", "class": "enabled"}';
+else
+	printf '{"text": ""}';
+fi
