d1937fd61aede41f975826e17ef48565f8cc089f
Rafael Ito <ito.rafael@gmail.com>
2024-10-17 11:33:07 -0300

    [waybar] mouse-warping: add enabled/disabled icon

--- waybar/scripts/mouse-warping.sh
+++ waybar/scripts/mouse-warping.sh
@@ -11,18 +11,18 @@ case "${CMD}" in
     "monitor")
         # set initial state
         if [[ "$MOUSE_WARPING" == "1" ]]; then
-            echo '{"text": "", "class": "enabled"}'
+            echo '{"text": "", "alt": "enabled", "class": "enabled"}'
         else
-            echo '{"text": ""}'
+            echo '{"text": "", "alt": "disabled", "class": "disabled"}'
         fi
 
         # infinite loop that changes text everytime the mouse warping file changes
         inotifywait --quiet --monitor --event close_write $FILE | while read; do
             MOUSE_WARPING=$(cat $FILE)
             if [[ "$MOUSE_WARPING" == "1" ]]; then
-                echo '{"text": "", "class": "enabled"}'
+                echo '{"text": "", "alt": "enabled", "class": "enabled"}'
             else
-                echo '{"text": ""}'
+                echo '{"text": "", "alt": "disabled", "class": "disabled"}'
             fi
             sleep 0.1
         done
