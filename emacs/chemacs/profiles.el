d3f4c376b036b2df26b54a1d93eac79dbb363b4e
Rafael Ito <ito.rafael@gmail.com>
2024-11-08 16:18:50 -0300

    [emacs] chemacs: add profiles.el config file

---
+++ emacs/chemacs/profiles.el
@@ -0,0 +1,3 @@
+(("default" . ((user-emacs-directory . "~/.config/emacs-efs")))
+ ("doom"    . ((user-emacs-directory . "~/.config/emacs-doom")
+              (env . (("DOOMDIR" . "~/.config/doom"))))))
