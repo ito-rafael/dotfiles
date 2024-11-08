b3eb8559ad6689fadbafac2628b9d514fd5d893e
Rafael Ito <ito.rafael@gmail.com>
2024-11-08 17:18:55 -0300

    [emacs] chemacs: set it to work with emacsclient (daemon)

--- emacs/chemacs/profiles.el
+++ emacs/chemacs/profiles.el
@@ -1,3 +1,10 @@
-(("default" . ((user-emacs-directory . "~/.config/emacs-efs")))
- ("doom"    . ((user-emacs-directory . "~/.config/emacs-doom")
-              (env . (("DOOMDIR" . "~/.config/doom"))))))
+(
+  ;; default EFS profile (Emacs From Scratch)
+  ("efs" . ((user-emacs-directory . "~/.config/emacs-efs")
+                (server-name . "efs")))
+
+  ;; Doom Emacs profile (Emacs distribution)
+   ("doom" . ((user-emacs-directory . "~/.config/emacs-doom")
+               (server-name . "doom")
+               (env . (("DOOMDIR" . "~/.config/doom")))))
+)
