6388256899d5288057e2078a412222e11a716fae
Rafael Ito <ito.rafael@gmail.com>
2024-12-04 10:17:21 -0300

    [waybar] scripts/kmonad: add support for --user systemctl flag

--- waybar/scripts/kmonad.sh
+++ waybar/scripts/kmonad.sh
@@ -1,11 +1,55 @@
 #!/usr/bin/env bash
+#
+# This script is meant to be used with Waybar to control and monitor KMonad global/user services.
+#
+#=================================================
+# help menu and usage message
+#=================================================
+usage="$(basename "$0") action [-h]
 
+where:
+    -h, --help       show this help text
+    action [--user]  action to be performed, can be one of the options:
+      \"start\"        start KMonad service with systemd
+      \"stop\"         stop KMonad service with systemd
+      \"restart\"      restart KMonad service with systemd
+      \"toggle\"       toggle (start/stop) KMonad service with systemd
+      \"monitor\"      prints KMonad service status everytime the file /tmp/kmonad-status.tmp is updated (meant to be used with Waybar)
+
+    Obs: if the flag \"--user\" is used, then systemd controls the user service. Otherwise, the global service is used as the target.
+"
+
+#=================================================
+# print help menu
+if [[ $1 == '-h' || $1 == '--help' ]]; then
+	printf "script to create, show, detach or destroy temporary scratchpad\n\n"
+	echo "$usage"
+	exit 0
+fi
+
+#=================================================
+# parse parameters
+#=================================================
+ACTION=$1
+SCOPE=$2
+
+case "${SCOPE}" in
+    "--user")
+        CMD="systemctl --user"
+        ;;
+    "")
+        CMD="sudo systemctl"
+        ;;
+    *)
+        echo "Error: parameter \"$SCOPE\" not supported"
+        exit 1
+        ;;
+esac
+
+#=================================================
 # read KMonad service status
 FILE="/tmp/kmonad-status.tmp"
 TIMEOUT=0.75
-
-# parse parameter
-CMD=$1
 
 # function to update text in status bar (Waybar)
 update_output () {
@@ -28,37 +72,38 @@ update_output () {
 
 # function to query KMonad service status and dump it into a file
 check_status () {
-    STATUS=$(systemctl is-active kmonad)
+    STATUS=$(systemctl --user is-active kmonad)
     update_output $STATUS
     echo $STATUS > $FILE
 }
+
 # parse requested action
-case "${CMD}" in
+case "${ACTION}" in
     "status")
         check_status
         ;;
     "start")
-        sudo systemctl start kmonad
+        $CMD start kmonad
         sleep $TIMEOUT
         check_status
         ;;
     "stop")
-        sudo systemctl stop kmonad
+        $CMD stop kmonad
         sleep $TIMEOUT
         check_status
         ;;
     "restart")
-        sudo systemctl restart kmonad
+        $CMD restart kmonad
         sleep $TIMEOUT
         check_status
         ;;
     "toggle")
-        if systemctl is-active --quiet kmonad; then
+        if systemctl --user is-active --quiet kmonad; then
             # kmonad active, stopping it...
-            sudo systemctl stop kmonad
+            $CMD stop kmonad
         else
             # kmonad inactive, starting it...
-            sudo systemctl start kmonad
+            $CMD start kmonad
         fi
         sleep $TIMEOUT
         check_status
@@ -67,7 +112,7 @@ case "${CMD}" in
         check_status
         # infinite loop that updates text everytime the mouse warping file changes
         inotifywait --quiet --monitor --event close_write $FILE | while read; do
-            STATUS=$(systemctl is-active kmonad)
+            STATUS=$(systemctl --user is-active kmonad)
             update_output $STATUS
             sleep 0.1
         done
