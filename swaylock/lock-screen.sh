8fa9562c90331dc4f0df34e5e2b3061af44f9e76
Rafael Ito <ito.rafael@gmail.com>
2024-12-20 13:40:39 -0300

    [swaylock] add support for task reminder (red background)

--- swaylock/lock-screen.sh
+++ swaylock/lock-screen.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+TODO_FILE='/tmp/todo-list.tmp'
+SCREENSHOT=$XDG_CONFIG_HOME/swaylock/screenshot.png
+
 #-----------------------------
 # before screen locking
 #-----------------------------
@@ -13,6 +16,20 @@ playerctl --all-players pause
 # dim LEDs of keyboard
 $XDG_CONFIG_HOME/scripts/keyboard-leds.sh dim
 
+# check if there are any reminders
+REMINDERS=$(cat $TODO_FILE | wc -l)
+if [[ $REMINDERS == "0" ]]; then
+    # if no reminders, take a simple screenshot
+    grim - | magick - $SCREENSHOT
+else
+    # if any reminders, take screenshot (grim's stdout) and apply a red background (ImageMagick's stdin)
+    grim - | magick -  \
+        -format png    \
+        -fill red      \
+        -colorize 80%  \
+        $SCREENSHOT
+fi
+
 #-----------------------------
 # screen locking
 #-----------------------------
@@ -20,8 +37,11 @@ $XDG_CONFIG_HOME/scripts/keyboard-leds.sh dim
 swaylock --config $XDG_CONFIG_HOME/swaylock/config
 
 #-----------------------------
-# after screen locking
+# after screen unlocking
 #-----------------------------
+
+# delete screenshot
+rm $SCREENSHOT
 
 # turn on keyboard LEDs
 $XDG_CONFIG_HOME/scripts/keyboard-leds.sh on
