b76e33a915911e39f61bdd711e9edb651da8651b
Rafael Ito <ito.rafael@gmail.com>
2025-08-06 17:52:21 -0300

    [scripts] menu_unicamp.py: create Menu class

--- scripts/menu_unicamp.py
+++ scripts/menu_unicamp.py
@@ -6,3 +6,19 @@ import datetime
 link = 'https://sistemas.prefeitura.unicamp.br/apps/cardapio/index.php'
 res = requests.get(link)
 soup = BeautifulSoup(res.text, 'html.parser')
+
+class Menu(object):
+    def __init__(self, main, menu_description):
+        # change text to title case
+        menu = menu_description.get_text(strip=True, separator="\n").splitlines()
+        self.menu = [self.title_case(item) for item in menu[:5]]
+        self.main = self.title_case(main)
+
+        # store attributes
+        self.base = self.menu[0]
+        self.side = self.menu[1]
+        self.salad = self.menu[2]
+        self.dessert = self.menu[3]
+        self.juice = self.menu[4]
+        self.obs = menu[5:8]
+        self.availability = menu[8:]
