c71a0c8ca4bf9ed4f5b85ae5ed0d05a6beed62f7
Rafael Ito <ito.rafael@gmail.com>
2025-09-25 08:38:13 -0300

    [scripts] hide-or-kill: add support for Translate scratchpad

--- scripts/hide-or-kill.sh
+++ scripts/hide-or-kill.sh
@@ -64,12 +64,14 @@ is_scratchpad=$($WM_CMD -t get_tree | jq -re '.. | select(type == "object") | se
     .'$PROP_PREFIX''$PROP' == "brave-calendar.google.com__-Default" or
     .'$PROP_PREFIX''$PROP' == "brave-chatgpt.com__-Default" or
     .'$PROP_PREFIX''$PROP' == "brave-music.youtube.com__-Default" or
+    .'$PROP_PREFIX''$PROP' == "brave-translate.google.com__-Default" or
     .'$PROP_PREFIX''$PROP' == "brave-web.whatsapp.com__-Default" or
     .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "music.youtube.com" or
     .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "web.whatsapp.com" or
     .'$PROP_PREFIX''$PROP' == "keymapp" or .'$PROP_PREFIX''$PROP' == "Keymapp" or
     .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "calendar.google.com" or
     .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "chatgpt.com" or
+    .'$PROP_PREFIX''$PROP' == "Brave-browser-beta" and .'$PROP_PREFIX''$INSTANCE' == "translate.google.com" or
     .'$PROP_PREFIX''$PROP' == "scrcpy" and .'$PROP_PREFIX''$CAPTION' == "dropdown_scrcpy_phone" or
     .'$PROP_PREFIX''$PROP' == "scrcpy" and .'$PROP_PREFIX''$CAPTION' == "dropdown_scrcpy_watch" or
     .'$ID_PROP' == '$TEMP_PID_1' or 
