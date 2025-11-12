c732529fec401d02c4e0ca9d3ad5b6b42d5e63a5
Rafael Ito <ito.rafael@gmail.com>
2025-11-12 17:33:13 -0300

    [archinstall] add documentation explaining how to run

--- archlinux/archinstall/archinstall.sh
+++ archlinux/archinstall/archinstall.sh
@@ -1,10 +1,16 @@
-wget https://geo.mirror.pkgbuild.com/iso/2025.10.01/archlinux-x86_64.iso
+archinstall \
+    --config-url https://raw.githubusercontent.com/ito-rafael/dotfiles/refs/heads/master/archlinux/archinstall/config.json \
+    --creds-url https://raw.githubusercontent.com/ito-rafael/dotfiles/refs/heads/master/archlinux/archinstall/credentials.json
 
-gpg --keyserver-options auto-key-retrieve --verify archlinux-<VERSION>-x86_64.iso.sig
+wget https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso
+
+gpg --keyserver-options auto-key-retrieve --verify archlinux-x86_64.iso.sig
 
 dd bs=4M if=path/to/archlinux.iso of=/dev/<SDX> status=progress oflag=sync
 
-archinstall --config-url https://github.com/ito-rafael/dotfiles/blob/master/arch/archinstall-ansible.json
+archinstall \
+    --config-url https://raw.githubusercontent.com/ito-rafael/dotfiles/refs/heads/master/archlinux/archinstall/config.json \
+    --creds-url https://raw.githubusercontent.com/ito-rafael/dotfiles/refs/heads/master/archlinux/archinstall/credentials.json
 
 findmnt -t btrfs -o TARGET,SOURCE,OPTIONS
 
@@ -13,3 +19,5 @@ cat /etc/fstab
 lsattr -d /var/lib/docker /var/lib/libvirt/images /swap/swapfile
 
 btrfs subvolume list / | grep -E "@snapshot-home/|@snapshot-root/"
+
+su - ansible --command "ANSIBLE_FORCE_COLOR=true ansible-pull --url https://github.com/ito-rafael/ansible-provision | tee /home/ansible/ansible-setup.log"
