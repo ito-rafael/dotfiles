4ff94ec90e4e870dba7c0f28331f873b4e71721b
Rafael Ito <ito.rafael@gmail.com>
2025-11-23 00:11:51 -0300

    [zsh] rootless: install m4

--- zsh/rootless-config.sh
+++ zsh/rootless-config.sh
@@ -2,3 +2,12 @@ cd
 mkdir local-build
 export PATH=/home/rafael.ito/local-build/bin:$PATH
 BUILD_PATH=$HOME/local-build
+
+cd
+wget ftp.gnu.org/gnu/m4/m4-latest.tar.gz
+tar -xvzf m4-latest.tar.gz
+rm m4-latest.tar.gz
+cd m4-*
+./configure --prefix=$BUILD_PATH
+make
+make install
