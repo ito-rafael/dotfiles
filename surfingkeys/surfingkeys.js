6958d00ec0022b0b3c0ed17969e93d34b9d11837
Rafael Ito <ito.rafael@gmail.com>
2025-11-25 19:34:12 -0300

    [surfingkeys] refactor all maps/unmaps

--- surfingkeys/surfingkeys.js
+++ surfingkeys/surfingkeys.js
@@ -1,48 +1,22 @@
 api.Hints.setCharacters('neiohtsrad');
 
-api.map("_h",  "h");
-api.map("_j",  "j");
-api.map("_k",  "k");
-api.map("_l",  "l");
-api.map("_S",  "S");
-api.map("_D",  "D");
-api.map("_E",  "E");
-api.map("_R",  "R");
-api.map("_I",  "I");
-api.map("_af", "af");
-
-api.unmap("h");
-api.unmap("j");
-api.unmap("k");
-api.unmap("l");
-api.unmap("S");
-api.unmap("D");
-api.unmap("E");
-api.unmap("R");
-api.unmap("I");
-api.unmap("af");
-
-api.map("n", "_h");
-api.map("e", "_j");
-api.map("i", "_k");
-api.map("o", "_l");
-
-api.map("N", "_S");
-api.map("O", "_D");
-
-api.map("E", "_R");
-api.map("I", "_E");
-
-api.map("F", "_af");
-
-api.unmap("_h");
-api.unmap("_j");
-api.unmap("_k");
-api.unmap("_l");
-api.unmap("_S");
-api.unmap("_D");
-api.unmap("_E");
-api.unmap("_R");
-api.unmap("_I");
-
 api.unmapAllExcept([], /localhost/);
+api.vmap("_h",  "h");
+api.vmap("_j",  "j");
+api.vmap("_k",  "k");
+api.vmap("_l",  "l");
+
+api.vunmap("h");
+api.vunmap("j");
+api.vunmap("k");
+api.vunmap("l");
+
+api.vmap("n",  "_h");
+api.vmap("e",  "_j");
+api.vmap("i",  "_k");
+api.vmap("o",  "_l");
+
+api.vunmap("_h");
+api.vunmap("_j");
+api.vunmap("_k");
+api.vunmap("_l");
