69a54b1b852ea504bf0432bcbcebdf6a20454bb4
Rafael Ito <ito.rafael@gmail.com>
2026-02-04 10:33:13 -0300

    [scripts] sway-scale: add script to change scale (zoom in/out)

---
+++ scripts/sway-scale.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+for cmd in jq bc swaymsg; do
+    if ! command -v $cmd &>/dev/null; then
+        echo "Error: '$cmd' is not installed."
+        exit 1
+    fi
+done
+
+show_help() {
+    echo "Usage:"
+    echo "  $(basename "$0") toggle      # Toggle between $SCALE_DEFAULT and $SCALE_ZOOM"
+    echo "  $(basename "$0") <number>    # Set scale to specific number (e.g.: 1.2)"
+    echo "  $(basename "$0") help        # Show this message"
+}
+
+SCALE_DEFAULT="1.0"
+SCALE_ZOOM="1.15"
+
+SCALE_MIN="0.5"
+SCALE_MAX="4.0"
+
+COMMAND="$1"
+
+validate_range() {
+    local target_scale=$1
+    # validate only positive number (integer or float)
+    if ! [[ "$target_scale" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        echo "Error: '$target_scale' is not a valid positive number."
+        exit 1
+    fi
+
+    # validate range
+    local is_valid=$(echo "$target_scale >= $SCALE_MIN && $target_scale <= $SCALE_MAX" | bc -l)
+
+    if [ "$is_valid" -eq 0 ]; then
+        echo "Error: Scale '$target_scale' is out of bounds."
+        echo "Please provide a scale between $SCALE_MIN and $SCALE_MAX."
+        exit 1
+    fi
+}
+
+set_scale() {
+    # get output names
+    OUTPUT_NAMES=$(swaymsg -t get_outputs | jq -r '.[].name')
+
+    local target_scale=$1
+    # loop through all outputs and apply the scale
+    for output in $OUTPUT_NAMES; do
+        echo "Setting $output to scale $target_scale..."
+        swaymsg output "$output" scale "$target_scale"
+    done
+}
+
+case "$COMMAND" in
+
+"help" | "-h" | "--help" | "")
+    show_help
+    exit 0
+    ;;
+
+"toggle")
+    # get current scale and decide the target one
+    CURRENT_SCALE=$(swaymsg -t get_outputs | jq -r '.[0].scale')
+    # toggle between default/zoomed predefined scales
+    IS_DEFAULT=$(echo "$CURRENT_SCALE == $SCALE_DEFAULT" | bc -l)
+    if [ "$IS_DEFAULT" -eq 1 ]; then
+        set_scale "$SCALE_ZOOM"
+    else
+        set_scale "$SCALE_DEFAULT"
+    fi
+    exit 0
+    ;;
+
+*)
+    validate_range "$COMMAND"
+    set_scale "$COMMAND"
+    exit 0
+    ;;
+
+esac
