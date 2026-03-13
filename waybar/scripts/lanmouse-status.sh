6a93506bfacf4c11da85721d23a46b8c034f9974
Rafael Ito <ito.rafael@gmail.com>
2026-03-13 09:07:25 -0300

    [waybar] scripts: add support for lan-mouse "active" status

--- waybar/scripts/lanmouse-status.sh
+++ waybar/scripts/lanmouse-status.sh
@@ -5,10 +5,12 @@ LANMOUSE_FILE="/tmp/lanmouse-status.tmp"
 # function to print lan-mouse status
 print_status() {
     LANMOUSE_STATUS=$(cat $LANMOUSE_FILE)
-    if [[ "$LANMOUSE_STATUS" == "connected" ]]; then
-        echo '{"text": "", "class": "connected"}'
-    elif [[ "$LANMOUSE_STATUS" == "disconnected" ]]; then
+    if [[ "$LANMOUSE_STATUS" == "disconnected" ]]; then
         echo '{"text": "", "class": "disconnected"}'
+    elif [[ "$LANMOUSE_STATUS" == "connected" ]]; then
+        echo '{"text": "", "class": "connected"}'
+    elif [[ "$LANMOUSE_STATUS" == "active" ]]; then
+        echo '{"text": "", "class": "active"}'
     else
         echo '{"text": "", "class": "unknown"}'
     fi
@@ -21,4 +23,28 @@ print_status
 inotifywait --quiet --monitor --event close_write $LANMOUSE_FILE | while read; do
     print_status
     sleep 0.1
+done
+
+
+# function to print current scale
+print_scale() {
+    SCALE=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .scale')
+    CURRENT_SCALE=$(swaymsg -t get_outputs | jq -r '.[0].scale')
+
+    IS_DEFAULT=$(echo "$CURRENT_SCALE == $SCALE_DEFAULT" | bc -l)
+    if [ "$IS_DEFAULT" -eq 1 ]; then
+        echo '{"text": "'$CURRENT_SCALE'", "alt": "default", "class": "default"}'
+    else
+        echo '{"text": "'$CURRENT_SCALE'", "alt": "zoom", "class": "zoom"}'
+    fi
+}
+
+# print init state
+print_scale
+
+# subscribe to output events
+swaymsg -m -t subscribe '["output"]' | \
+while read -r event; do
+    # when any output event happens, refresh the data
+    print_scale
 done
