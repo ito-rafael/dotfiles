f1a6631191a2b9ad05ac4a6725ab86d5a7596eed
Rafael Ito <ito.rafael@gmail.com>
2026-03-21 12:05:22 -0300

    [lightdm] lightdm-cleanup: replace pkill cmd with cleanup script

---
+++ lightdm/lightdm-cleanup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# kill the lan-mouse daemon if it is running
+pkill -u lightdm lan-mouse
+
+# kill the background wait-loop from the greeter script (lightdm-lanmouse.sh) if it's still running
+pkill -u lightdm -f "lightdm-lan-mouse.sh"
+pkill -u lightdm -f "sleep"
+
+# force a successful exit code (0) so LightDM does't abort the login
+exit 0
