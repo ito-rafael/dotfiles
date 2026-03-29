23e1f7a3db8e70bf72d421851352fb3792b03ffc
Rafael Ito <ito.rafael@gmail.com>
2026-03-29 07:17:21 -0300

    [lightdm] lightdm-cleanup: rename lightdm lan-mouse script

--- lightdm/lightdm-cleanup.sh
+++ lightdm/lightdm-cleanup.sh
@@ -4,7 +4,7 @@
 pkill -u lightdm lan-mouse
 
 # kill the background wait-loop from the greeter script (lightdm-lanmouse.sh) if it's still running
-pkill -u lightdm -f "lightdm-lan-mouse.sh"
+pkill -u lightdm -f "lightdm-lanmouse.sh"
 pkill -u lightdm -f "sleep"
 
 # force a successful exit code (0) so LightDM does't abort the login
