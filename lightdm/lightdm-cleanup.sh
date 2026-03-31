8dff9dfe83c5e5d7875f04dca7c308112703ebdf
Rafael Ito <ito.rafael@gmail.com>
2026-03-31 19:31:51 -0300

    [lightdm] lightdm-cleanup: append "|| true" in the pkill commands (avoid error code)

--- lightdm/lightdm-cleanup.sh
+++ lightdm/lightdm-cleanup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # kill the lan-mouse daemon if it is running
-pkill -u lightdm lan-mouse
+pkill -u lightdm lan-mouse || true
 
 # kill the background wait-loop from the greeter script (lightdm-lanmouse.sh) if it's still running
-pkill -u lightdm -f "lightdm-lanmouse.sh"
-pkill -u lightdm -f "sleep"
+pkill -u lightdm -f "lightdm-lanmouse.sh" || true
+pkill -u lightdm -f "sleep" || true
 
 # force a successful exit code (0) so LightDM does't abort the login
 exit 0
