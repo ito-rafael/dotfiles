7712b878b5a6cb6d44bd249a1b070c16d62e83b3
Rafael Ito <ito.rafael@gmail.com>
2026-04-13 21:03:21 -0300

    [polybar] modules: add terminal scratchpad

---
+++ polybar/modules/scratchpad-terminal.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# identify session (i3wm/Sway) and set vars accordingly
+case "${XDG_SESSION_TYPE}" in
+    "x11")
+        WM_CMD="i3-msg"
+        PROP_PREFIX="window_properties."
+        PROP="class"
+        CAPTION="title"
+        INSTANCE="instance"
+        ;;
+    "wayland")
+        WM_CMD="swaymsg"
+        PROP="app_id"
+        CAPTION="name"
+        ;;
+    "tty")
+        exit 0
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
+# parse parameters
+COLOR=$1
+GRAY=555
+
+# check if scratchpad exists
+SCRATCHPAD=$($WM_CMD -t get_tree | jq -re '.. | select(type == "object") | select(
+    .'$PROP_PREFIX''$PROP' == "dropdown_terminal"
+    )')
+
+# decide if icon will be displayed in color or black/white
+if [[ $SCRATCHPAD ]]; then
+    echo "%{F#$COLOR}%{F-}"
+else
+    echo "%{F#$GRAY}%{F-}"
+fi
