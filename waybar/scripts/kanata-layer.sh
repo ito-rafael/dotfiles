c2db43990d399e6771cc72cfa808924a0f989992
Rafael Ito <ito.rafael@gmail.com>
2026-04-16 16:21:59 -0300

    [waybar] scripts: add layers short nomes for kanata-layer script

--- waybar/scripts/kanata-layer.sh
+++ waybar/scripts/kanata-layer.sh
@@ -13,12 +13,38 @@ echo '{"text": "default", "class": "default"}'
 # connect to Kanata's TCP server and keep the stream open
 exec 3<>/dev/tcp/127.0.0.1/54338
 
-# filter the JSON stream for layer changes and format it for Waybar
+# filter, map, and format the JSON stream
 jq --unbuffered -c '
+  # define the dictionary mapping ("kanata_name": "Waybar_name")
+  {
+    "colemak":         "col",
+    "number":          "num",
+    "symbol":          "sym",
+    "obs":             "obs",
+    "obs-num":         "obn",
+    "window-manager":  "wm",
+    "navigation":      "nav",
+    "out1":            "out1",
+    "out2":            "out2",
+    "out3":            "out2",
+    "media":           "med",
+    "luminosity":      "lum",
+    "mouse":           "rat",
+    "misc":            "misc",
+    "youtube-speed":   "yt",
+    "function-keys-1": "fn1",
+    "function-keys-2": "fn2",
+  } as $names |
+
   select(.LayerChange != null) |
   {
-    text: .LayerChange.new,
+    # look up the short name in the dictionary, fallback to the original name if not found
+    text: ($names[.LayerChange.new] // .LayerChange.new),
+
+    # keep the tooltip showing the full original name
     tooltip: ("Active Layer: " + .LayerChange.new),
+
+    # keep the class as the original name so your CSS styling still works
     class: .LayerChange.new
   }
 ' <&3
