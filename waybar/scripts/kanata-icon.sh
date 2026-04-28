7deb6e46f554b0fe0c9ee7c452d3fc45a958956e
Rafael Ito <ito.rafael@gmail.com>
2026-04-28 10:50:38 -0300

    [waybar] scripts: add hide support for kanata-{icon,layer}.sh

---
+++ waybar/scripts/kanata-icon.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# check if the systemd user service is active
+if ! systemctl --user is-active --quiet kanata; then
+    echo ""  # output nothing to hide the image
+    exit 0
+fi
+
+# if active, output the path to the image
+echo /home/rafael/.config/waybar/icon/kanata.svg
--- waybar/scripts/kanata-layer.sh
+++ waybar/scripts/kanata-layer.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+# check if the systemd user service is active
+if ! systemctl --user is-active --quiet kanata; then
+    exit 0  # outputting nothing and exiting hides the module in Waybar
+fi
+
 # ensure jq is installed
 if ! command -v jq &> /dev/null; then
     echo '{"text": "jq missing"}'
     exit 1
 fi
 
+# attempt to connect to kanata's TCP server
+# if the port isn't open yet, exit to avoid a "stuck" module
+if ! exec 3<>/dev/tcp/127.0.0.1/54338 2>/dev/null; then
+    exit 0
+fi
+
 # output a default fallback state upon Waybar startup
 # (kanata only broadcasts on layer *changes*, so this prevents an empty module on boot)
 echo '{"text": "default", "class": "default"}'
-
-# connect to Kanata's TCP server and keep the stream open
-exec 3<>/dev/tcp/127.0.0.1/54338
 
 # filter, map, and format the JSON stream
 jq --unbuffered -c '
