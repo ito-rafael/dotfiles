45f2db31d2b9a9c9bb1a8ab6298fb274149e85e4
Rafael Ito <ito.rafael@gmail.com>
2026-05-06 23:05:23 -0300

    [waybar] scripts: add loading state in pacman.sh updates check

--- waybar/scripts/pacman.sh
+++ waybar/scripts/pacman.sh
@@ -7,10 +7,24 @@ UPDATES=$(checkupdates | wc -l)
 LEVEL_1="100"
 LEVEL_2="200"
 
-if [ $UPDATES -lt $LEVEL_1 ]; then
-    echo '{"text": "'$UPDATES'", "alt": "low", "class": "low"}'
-elif [ $UPDATES -ge $LEVEL_1 ] && [ $UPDATES -lt $LEVEL_2 ]; then
-    echo '{"text": "'$UPDATES'", "alt": "medium", "class": "medium"}'
-else
-    echo '{"text": "'$UPDATES'", "alt": "high", "class": "high"}'
-fi
+# define the interval
+DELAY=600  # 600 seconds = 10 minutes
+
+while true; do
+    # output the loading state
+    echo '{"text": "", "alt": "loading", "class": "loading"}'
+
+    # check for available updates
+    UPDATES=$(checkupdates 2>/dev/null | wc -l)
+
+    # output the final state
+    if [ "$UPDATES" -lt "$LEVEL_1" ]; then
+        echo '{"text": "'$UPDATES'", "alt": "low", "class": "low"}'
+    elif [ "$UPDATES" -ge "$LEVEL_1" ] && [ "$UPDATES" -lt "$LEVEL_2" ]; then
+        echo '{"text": "'$UPDATES'", "alt": "medium", "class": "medium"}'
+    else
+        echo '{"text": "'$UPDATES'", "alt": "high", "class": "high"}'
+    fi
+
+    sleep $DELAY
+done
