6689088c6d2da127db9061d7fb99351be4f4ceba
Rafael Ito <ito.rafael@gmail.com>
2026-05-12 16:29:28 -0300

    [waybar] scripts: add loading support for ansible-provision.sh

--- waybar/scripts/ansible-provision.sh
+++ waybar/scripts/ansible-provision.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
-# run Ansible in check mode (--check) and force JSON output
-# obs: we redirect stderr to /dev/null so warnings don't corrupt the JSON string
-OUTPUT=$(sudo -u ansible /home/ansible/git/dotfiles/waybar/scripts/ansible-check.sh 2>/dev/null)
+# define the interval
+DELAY=3600  # 3600 seconds = 1 hour
 
-# extract the sum of all 'changed' stats across all hosts
-CHANGED=$(echo "$OUTPUT" | jq -r '[.stats[].changed] | add')
+while true; do
+    # output the loading state
+    echo '{"text": " ", "alt": "loading", "class": "loading"}'
 
-# catch errors (e.g., if Ansible failed to run entirely)
-if [ -z "$CHANGED" ] || [ "$CHANGED" == "null" ]; then
-    # return a Waybar JSON error state
-    echo '{"text": "⚠ err", "tooltip": "Ansible check failed to execute", "class": "error"}'
-    exit 1
-fi
+    # run Ansible in check mode (--check) and force JSON output
+    # obs: we redirect stderr to /dev/null so warnings don't corrupt the JSON string
+    OUTPUT=$(sudo -u ansible /home/ansible/git/dotfiles/waybar/scripts/ansible-check.sh 2>/dev/null)
 
-# format the output for Waybar based on the state
-if [ "$CHANGED" -gt 0 ]; then
-    # out of sync! send "warning" as the alt state
-    echo '{"text": "'"$CHANGED"'", "alt": "out-of-sync", "tooltip": "'"$CHANGED"' tasks pending sync", "class": "out-of-sync"}'
-else
-    # perfect sync! send "synced" as the alt state
-    echo '{"text": "0", "alt": "synced", "tooltip": "System is fully synced", "class": "synced"}'
-fi
+    # extract the sum of all 'changed' stats across all hosts
+    CHANGED=$(echo "$OUTPUT" | jq -r '[.stats[].changed] | add')
+
+    # catch errors (e.g., if Ansible failed to run entirely)
+    if [ -z "$CHANGED" ] || [ "$CHANGED" == "null" ]; then
+        # return a Waybar JSON error state
+        echo '{"text": "⚠ err", "tooltip": "Ansible check failed to execute", "class": "error"}'
+    elif [ "$CHANGED" -gt 0 ]; then
+        # out of sync! send "warning" as the alt state
+        echo '{"text": "'"$CHANGED"'", "alt": "out-of-sync", "tooltip": "'"$CHANGED"' tasks pending sync", "class": "out-of-sync"}'
+    else
+        # perfect sync! send "synced" as the alt state
+        echo '{"text": "0", "alt": "synced", "tooltip": "System is fully synced", "class": "synced"}'
+    fi
+
+    sleep $DELAY
+done
