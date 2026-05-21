dd5663d4305cb87e04ecfa388d8e57a845675112
Rafael Ito <ito.rafael@gmail.com>
2026-05-21 10:37:05 -0300

    [waybar] scripts: update kanata-icon to monitor user systemd service

--- waybar/scripts/kanata-icon.sh
+++ waybar/scripts/kanata-icon.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-# check if the systemd user service is active
-if ! systemctl --user is-active --quiet kanata; then
-    echo ""  # output nothing to hide the image
-    exit 0
-fi
+# function to check state and output json
+update_state() {
+    if systemctl --user is-active --quiet kanata; then
+        # service active
+        echo '{"text": " ", "class": "active", "tooltip": "kanata is running"}'
+    else
+        # service inactive
+        echo '{"text": " ", "class": "inactive", "tooltip": "kanata is stopped"}'
+    fi
+}
 
-# if active, output the path to the image
-echo "/home/rafael/.config/waybar/icon/kanata.svg"
+# get initial state
+update_state
+
+# block and listen for systemd state changes via D-Bus
+# listen specifically for changes to the kanata.service on the user bus
+dbus-monitor --session "type='signal',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged',arg0='org.freedesktop.systemd1.Unit',path='/org/freedesktop/systemd1/unit/kanata_2eservice'" |
+while read -r line; do
+    # when D-Bus spits out a change for Kanata, update Waybar
+    # (check for the 'ActiveState' string to ensure it's a real start/stop event)
+    if echo "$line" | grep -q "ActiveState"; then
+        update_state
+    fi
+done
