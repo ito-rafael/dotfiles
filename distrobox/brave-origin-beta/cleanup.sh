457c5af03fa5317cbf8148eba55db10ca4d51720
Rafael Ito <ito.rafael@gmail.com>
2026-05-26 09:35:50 -0300

    [distrobox] brave-origin-beta: add cleanup.sh script to rebuild + configure image

---
+++ distrobox/brave-origin-beta/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+distrobox rm -f brave-origin-beta
+podman build -t brave-origin-beta -f Dockerfile ../../web-browser-extensions/
+distrobox create --name brave-origin-beta --image brave-origin-beta
+#distrobox enter brave-origin-beta -- sh -c 'brave-origin-beta --skip-origin-startup-dialog --headless=new --no-first-run > /dev/null 2>&1 & sleep 15 && pkill -15 -f brave-origin-beta'
+distrobox enter brave-origin-beta -- python3 /usr/local/bin/allow_user_scripts.py
+distrobox enter brave-origin-beta -- python3 /usr/local/bin/surfingkeys_advanced_mode.py
+distrobox enter brave-origin-beta -- python3 /usr/local/bin/surfingkeys_load_settings.py
