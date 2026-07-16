d8f424654239a18bb84f74199fc637195b49c411
Rafael Ito <ito.rafael@gmail.com>
2026-07-16 12:33:32 -0300

    [semaphore] gui: fix spaces

--- semaphore/gui.py
+++ semaphore/gui.py
@@ -34,14 +34,19 @@ class AnsibleProvisionApp(Gtk.ApplicationWindow):
         main_box.set_margin_end(20)
         self.set_child(main_box)
 
+        # SINGLE GRID setup
+        grid = Gtk.Grid(row_spacing=15, column_spacing=12)
+        grid.set_halign(Gtk.Align.CENTER) # Centers the tight form in the window
+        main_box.append(grid)
+
         # Header Box
-        header_box = Gtk.CenterBox(margin_bottom=15)
+        header_box = Gtk.CenterBox()
 
         # left logo
         left_logo = Gtk.Picture.new_for_filename("../icon/ansible.png")
         left_logo.set_size_request(50, 50)
         left_logo.set_content_fit(Gtk.ContentFit.CONTAIN)
-        left_logo.set_margin_start(15) # Adds space to the left of the logo
+        left_logo.set_margin_start(15)
         header_box.set_start_widget(left_logo)
 
         # title text
@@ -53,100 +58,92 @@ class AnsibleProvisionApp(Gtk.ApplicationWindow):
         right_logo = Gtk.Picture.new_for_filename("../icon/semaphore-ui.png")
         right_logo.set_size_request(50, 50)
         right_logo.set_content_fit(Gtk.ContentFit.CONTAIN)
-        right_logo.set_margin_end(15) # Adds space to the right of the logo
+        right_logo.set_margin_end(15)
         header_box.set_end_widget(right_logo)
 
-        main_box.append(header_box)
+        # Spanning 4 columns forces the header to match the width of the form inputs!
+        grid.attach(header_box, 0, 0, 4, 1)
 
-        # TOP GRID
-        self.top_grid = Gtk.Grid(row_spacing=15, column_spacing=12)
-        # FILL forces the grid to stretch the full width of the window
-        self.top_grid.set_halign(Gtk.Align.FILL)
-
-        # hosts (h) - Row 0
-        self.top_grid.attach(Gtk.Label(label="Hosts (h)", halign=Gtk.Align.START), 0, 0, 1, 1)
+        # hosts (h) - Row 1
+        grid.attach(Gtk.Label(label="Hosts (h)", halign=Gtk.Align.START), 0, 1, 1, 1)
         self.hosts_drop = Gtk.DropDown.new(Gtk.StringList.new(["localhost", "LBiC", "homelab", "rootless"]))
         self.hosts_drop.connect("notify::selected", self.update_command)
-        self.top_grid.attach(self.hosts_drop, 1, 0, 1, 1)
+        grid.attach(self.hosts_drop, 1, 1, 1, 1)
 
-        # strategy (m) - Row 3
-        self.top_grid.attach(Gtk.Label(label="Strategy (m)", halign=Gtk.Align.START), 0, 3, 1, 1)
+        # strategy (m) - Row 4
+        grid.attach(Gtk.Label(label="Strategy (m)", halign=Gtk.Align.START), 0, 4, 1, 1)
         self.strategy_drop = Gtk.DropDown.new(Gtk.StringList.new(["Mitogen", "Linear"]))
         self.strategy_drop.connect("notify::selected", self.update_command)
-        self.top_grid.attach(self.strategy_drop, 1, 3, 1, 1)
+        grid.attach(self.strategy_drop, 1, 4, 1, 1)
 
-        # tags (t) - Row 1
-        self.top_grid.attach(Gtk.Label(label="Tags (t)", halign=Gtk.Align.START), 0, 1, 1, 1)
+        # tags (t) - Row 2
+        grid.attach(Gtk.Label(label="Tags (t)", halign=Gtk.Align.START), 0, 2, 1, 1)
         self.tags_entry = Gtk.Entry(placeholder_text="e.g. brave")
         self.tags_entry.connect("changed", self.update_command)
-        self.top_grid.attach(self.tags_entry, 1, 1, 1, 1)
+        grid.attach(self.tags_entry, 1, 2, 1, 1)
 
-        # skip tags (s) - Row 2
-        self.top_grid.attach(Gtk.Label(label="Skip tags (s)", halign=Gtk.Align.START), 0, 2, 1, 1)
+        # skip tags (s) - Row 3
+        grid.attach(Gtk.Label(label="Skip tags (s)", halign=Gtk.Align.START), 0, 3, 1, 1)
         self.skip_tags_entry = Gtk.Entry(placeholder_text="e.g. emacs")
         self.skip_tags_entry.connect("changed", self.update_command)
-        self.top_grid.attach(self.skip_tags_entry, 1, 2, 1, 1)
+        grid.attach(self.skip_tags_entry, 1, 3, 1, 1)
 
-        # vertical separator
+        # vertical separator spans Rows 1 to 4
         v_sep = Gtk.Separator(orientation=Gtk.Orientation.VERTICAL)
-        # hexpand forces the separator's column to absorb all leftover space!
-        v_sep.set_hexpand(True)
-        # Centers the 1px line directly inside that newly expanded empty space
-        v_sep.set_halign(Gtk.Align.CENTER)
-        self.top_grid.attach(v_sep, 2, 0, 1, 4)
+        v_sep.set_halign(Gtk.Align.CENTER) # Prevents the rectangle! Keeps it 1px wide in the center.
+        v_sep.set_hexpand(True) # Makes this column act as a spring to absorb empty space.
+        grid.attach(v_sep, 2, 1, 1, 4)
 
-        # options label - Row 0
-        self.top_grid.attach(Gtk.Label(label="<b>Options:</b>", use_markup=True, halign=Gtk.Align.START), 3, 0, 1, 1)
+        # options label - Row 1
+        grid.attach(Gtk.Label(label="<b>Options:</b>", use_markup=True, halign=Gtk.Align.START), 3, 1, 1, 1)
 
-        # dry run (c) - Row 1
+        # dry run (c) - Row 2
         self.dry_run_check = Gtk.CheckButton(label="Dry run (c)")
         self.dry_run_check.connect("toggled", self.update_command)
-        self.top_grid.attach(self.dry_run_check, 3, 1, 1, 1)
+        grid.attach(self.dry_run_check, 3, 2, 1, 1)
 
-        # diff (d) - Row 2
+        # diff (d) - Row 3
         self.diff_check = Gtk.CheckButton(label="Diff (d)")
         self.diff_check.connect("toggled", self.update_command)
-        self.top_grid.attach(self.diff_check, 3, 2, 1, 1)
+        grid.attach(self.diff_check, 3, 3, 1, 1)
 
-        # debug (D) - Row 3
+        # debug (D) - Row 4
         self.debug_check = Gtk.CheckButton(label="Debug (D)")
         self.debug_check.connect("toggled", self.update_command)
-        self.top_grid.attach(self.debug_check, 3, 3, 1, 1)
+        grid.attach(self.debug_check, 3, 4, 1, 1)
 
-        # Append the tightly-packed top grid to the main box
-        main_box.append(self.top_grid)
-
-        # horizontal separator
-        main_box.append(Gtk.Separator(margin_top=5, margin_bottom=5))
-
-        # BOTTOM GRID (For wide, stretching elements)
-        self.bottom_grid = Gtk.Grid(row_spacing=15, column_spacing=12)
-
-        # start at task (a) - Row 0
-        self.bottom_grid.attach(Gtk.Label(label="Start at task (a)", halign=Gtk.Align.START), 0, 0, 1, 1)
+        # start at task (a) - Row 5
+        task_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=12)
+        task_box.append(Gtk.Label(label="Start at task (a)", halign=Gtk.Align.START))
         self.start_task_entry = Gtk.Entry(placeholder_text="Search tasks...", hexpand=True)
         self.start_task_entry.connect("changed", self.update_command)
-        self.bottom_grid.attach(self.start_task_entry, 1, 0, 1, 1)
+        task_box.append(self.start_task_entry)
 
-        # command - Row 1
-        self.bottom_grid.attach(Gtk.Label(label="Command:", halign=Gtk.Align.START), 0, 1, 1, 1)
+        grid.attach(task_box, 0, 5, 4, 1)
+
+        # horizontal separator - Row 6
+        h_sep = Gtk.Separator(margin_top=5, margin_bottom=5)
+        grid.attach(h_sep, 0, 6, 4, 1)
+
+        # command - Row 7
+        cmd_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=12)
+        cmd_box.append(Gtk.Label(label="Command:", halign=Gtk.Align.START))
         self.command_entry = Gtk.Entry(hexpand=True)
         self.command_entry.set_editable(False) # Read-only
         self.command_entry.add_css_class("monospace")
         self.command_entry.add_css_class("small-cmd") # Applies our smaller font
-        self.bottom_grid.attach(self.command_entry, 1, 1, 1, 1)
+        cmd_box.append(self.command_entry)
 
-        # Append the stretching bottom grid to the main box
-        main_box.append(self.bottom_grid)
+        grid.attach(cmd_box, 0, 7, 4, 1)
 
-        # left: Mode Indicator perfectly aligned in Col 0 - Row 2 of bottom_grid
+        # left: Mode Indicator - Row 8
         self.mode_label = Gtk.Label(label="NORMAL")
         self.mode_label.add_css_class("dim-label") # GTK's native class for discrete, faded text
         self.mode_label.set_halign(Gtk.Align.START)
         self.mode_label.set_margin_top(10)
-        self.bottom_grid.attach(self.mode_label, 0, 2, 1, 1)
+        grid.attach(self.mode_label, 0, 8, 1, 1)
 
-        # center: Action buttons wrapped in a Box
+        # center: Action buttons - Row 8
         btn_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
         btn_box.set_halign(Gtk.Align.CENTER) # Centers the buttons relative to the inputs above
         btn_box.set_margin_top(10)
@@ -161,7 +158,7 @@ class AnsibleProvisionApp(Gtk.ApplicationWindow):
         btn_box.append(launch_btn)
 
         # Spanning columns 1 to 3 underneath the input fields
-        self.bottom_grid.attach(btn_box, 1, 2, 3, 1)
+        grid.attach(btn_box, 1, 8, 3, 1)
 
         # keybindings (event controller)
         key_controller = Gtk.EventControllerKey.new()
