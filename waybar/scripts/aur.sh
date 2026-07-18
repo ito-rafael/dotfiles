c4edf6407a119c9f2ab4f9d3971ecf5f79c53781
Rafael Ito <ito.rafael@gmail.com>
2026-07-17 22:27:20 -0300

    [waybar] scripts: rename paru.sh to aur.sh (+add another level)

--- waybar/scripts/aur.sh
+++ waybar/scripts/aur.sh
@@ -2,7 +2,8 @@
 
 # define levels for AUR updates
 LEVEL_1="1"
-LEVEL_2="5"
+LEVEL_2="3"
+LEVEL_3="5"
 
 # define the interval
 DELAY=600  # 600 seconds = 10 minutes
@@ -20,8 +21,10 @@ while true; do
 
     # output the final state
     if [ "$UPDATES" -lt "$LEVEL_1" ]; then
-        echo '{"text": "'"$UPDATES"'", "alt": "low", "class": "low"}'
+        echo '{"text": "'"$UPDATES"'", "alt": "zero", "class": "zero"}'
     elif [ "$UPDATES" -ge "$LEVEL_1" ] && [ "$UPDATES" -lt "$LEVEL_2" ]; then
+        echo '{"text": "'"$UPDATES"'", "alt": "low", "class": "low"}'
+    elif [ "$UPDATES" -ge "$LEVEL_2" ] && [ "$UPDATES" -lt "$LEVEL_3" ]; then
         echo '{"text": "'"$UPDATES"'", "alt": "medium", "class": "medium"}'
     else
         echo '{"text": "'"$UPDATES"'", "alt": "high", "class": "high"}'
