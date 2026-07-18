f9698bc6f5c0a7ca9669d52303f3ef7c1f4fff21
Rafael Ito <ito.rafael@gmail.com>
2026-07-17 21:05:29 -0300

    [waybar] scripts: add paru.sh to check for AUR package updates

---
+++ waybar/scripts/paru.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# define levels for AUR updates
+LEVEL_1="1"
+LEVEL_2="5"
+
+# define the interval
+DELAY=600  # 600 seconds = 10 minutes
+
+while true; do
+    # output the loading state
+    echo '{"text": "", "alt": "loading", "class": "loading"}'
+
+    # check for available AUR updates
+    # - "Qua":
+    #   - "Q": query
+    #   - "u": upgrades
+    #   - "a": AUR
+    UPDATES=$(paru -Qua 2>/dev/null | wc -l)
+
+    # output the final state
+    if [ "$UPDATES" -lt "$LEVEL_1" ]; then
+        echo '{"text": "'"$UPDATES"'", "alt": "low", "class": "low"}'
+    elif [ "$UPDATES" -ge "$LEVEL_1" ] && [ "$UPDATES" -lt "$LEVEL_2" ]; then
+        echo '{"text": "'"$UPDATES"'", "alt": "medium", "class": "medium"}'
+    else
+        echo '{"text": "'"$UPDATES"'", "alt": "high", "class": "high"}'
+    fi
+
+    sleep $DELAY
+done
