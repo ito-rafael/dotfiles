bd18d8f4f20fe3e71b14e296215f801ef8e3b2f0
Rafael Ito <ito.rafael@gmail.com>
2026-07-29 19:05:13 -0300

    [dunst] sound-dispatch: add custom sound for dropdown_pacman

--- dunst/sound-dispatch.sh
+++ dunst/sound-dispatch.sh
@@ -4,7 +4,9 @@ APPNAME="$1"
 
 case "$APPNAME" in
     "wasistlos")
-        paplay ~/.config/dunst/whatsapp.mp3 ;;
+        paplay ~/.config/dunst/sound/whatsapp.mp3 ;;
+    "dropdown_pacman")
+        paplay ~/.config/dunst/sound/pacman-waka.mp3 ;;
     *)
         paplay /usr/share/sounds/freedesktop/stereo/bell.oga ;;
 esac
