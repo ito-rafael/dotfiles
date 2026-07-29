dbc707601a444d5b13fad131e07e1dd1c858171f
Rafael Ito <ito.rafael@gmail.com>
2026-07-29 19:05:23 -0300

    [dunst] sound-dispatch: add custom sound for dropdown_ansible

--- dunst/sound-dispatch.sh
+++ dunst/sound-dispatch.sh
@@ -7,6 +7,8 @@ case "$APPNAME" in
         paplay ~/.config/dunst/sound/whatsapp.mp3 ;;
     "dropdown_pacman")
         paplay ~/.config/dunst/sound/pacman-waka.mp3 ;;
+    "dropdown_ansible")
+        paplay ~/.config/dunst/sound/r2d2.mp3 ;;
     *)
         paplay /usr/share/sounds/freedesktop/stereo/bell.oga ;;
 esac
