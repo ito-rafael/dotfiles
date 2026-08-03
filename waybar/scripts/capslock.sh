56886856c9cb8e7a103242e6736457770ffba605
Rafael Ito <ito.rafael@gmail.com>
2026-08-03 16:42:09 -0300

    [waybar] scripts/capslock: dynamically change bar CSS color with Caps Lock

--- waybar/scripts/capslock.sh
+++ waybar/scripts/capslock.sh
@@ -8,10 +8,14 @@ inotifywait --quiet --monitor --event close_write $CAPSLOCK_FILE | while read; d
     # display "Caps Lock" text, if it is enabled
     CAPSLOCK=$(cat $CAPSLOCK_FILE)
     if [[ "$CAPSLOCK" == "1" ]]; then
-        echo '{"text": "  Caps Lock  ", "class": "enabled"}'
+        #echo '{"text": "  Caps Lock  ", "class": "enabled"}'
+        echo '{"text": "", "class": "enabled"}'
+        # change Waybar color to mint green
+        echo '@define-color dynamic_bg rgba(151, 225, 173, 0.75);' > ~/.config/waybar/dynamic-bg.css
     else
         echo '{"text": ""}'
+        # reset Waybar color
+        echo '@define-color dynamic_bg rgba(43, 48, 59, 0.5);' > ~/.config/waybar/dynamic-bg.css
     fi
     sleep 0.1
 done
-
