ca32e8a61f0f1a1d064ccece3fac7872423a9595
Rafael Ito <ito.rafael@gmail.com>
2026-08-05 14:02:27 -0300

    [eww] osd: add text for {Dark,Light} mode in theme OSD

--- eww/osd.sh
+++ eww/osd.sh
@@ -92,10 +92,12 @@ theme)
         # for a binary toggle, "0" looks good on the progress bar to represent "dark/off"
         VALUE=0
         ICON="/home/rafael/.config/icon/emoji-new-moon.svg"
+        TEXT="Dark Mode Enabled"
     else
         # 100 fills the bar to represent "light/on"
         VALUE=100
         ICON="/home/rafael/.config/icon/emoji-sun.svg"
+        TEXT="Light Mode Enabled"
     fi
     ;;
 
@@ -107,7 +109,7 @@ theme)
 esac
 
 # update generic variables in eww.yuck
-eww update osd_value="$VALUE" osd_icon="$ICON"
+eww update osd_value="$VALUE" osd_icon="$ICON" osd_text="${TEXT:-}"
 
 PID_FILE="/tmp/eww-osd.pid"
 LOCK_DIR="/tmp/eww-osd.lock"
